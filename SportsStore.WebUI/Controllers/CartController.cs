febc83dd0ecde9a9f04db78de91cae6efce32a27
Rochelle Gorbea <Rochelle.gorbea@outlook.com>
2019-04-05 12:33:00 -0700

    Initial

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -13,53 +13,70 @@ namespace SportsStore.WebUI.Controllers
     public class CartController : Controller
     {
 		private IProductsRepository repository;
+		private IOrderProcessor orderProcessor;
 
-		public CartController(IProductsRepository repo)
+		public CartController(IProductsRepository repo, IOrderProcessor proc)
 		{
 			repository = repo;
+			orderProcessor = proc;
 		}
 
-		public ViewResult Index(string returnUrl)
+		public ViewResult Index(Cart cart, string returnUrl)
 		{
 			return View(new CartIndexViewModel
 			{
-				Cart = GetCart(),
-				ReturnUrl = returnUrl
+				ReturnUrl = returnUrl,
+				Cart = cart
 			});
 		}
 
-		public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+		public RedirectToRouteResult AddToCart(Cart cart, int productID, string returnUrl)
 		{
 			Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
 
 			if(product!=null)
 			{ 
-				GetCart().AddItem(product, 1);
+				cart.AddItem(product,1);
 			}
 			return RedirectToAction("Index", new { returnUrl });
 		}
 
-		public RedirectToRouteResult RemoveFromCart(int productID, string returnUrl)
+		public RedirectToRouteResult RemoveFromCart(Cart cart, int productID, string returnUrl)
 		{
 			Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
 
 			if (product != null)
 			{
-				GetCart().RemoveLine(product);
+				cart.RemoveLine(product);
 			}
 			return RedirectToAction("Index", new { returnUrl });
 		}
 
-		private Cart GetCart()
+		public PartialViewResult Summary(Cart cart)
 		{
-			Cart cart = (Cart)Session["Cart"];
-			if (cart == null)
-			{
-				cart = new Cart();
-				Session["Cart"] = cart;
-			}
-			return cart;
+			return PartialView(cart);
 		}
 
+		public ViewResult Checkout()
+		{
+			return View(new ShippingDetails());
+		}
+
+		[HttpPost]
+		public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+		{
+			if (cart.Lines.Count() == 0) {
+				ModelState.AddModelError("", "Sorry, your cart is empty!");
+			}
+			
+			if (ModelState.IsValid)
+			{
+				orderProcessor.ProcessOrder(cart, shippingDetails);
+				cart.Clear();
+				return View("Completed");
+			}
+			else { return View(shippingDetails);
+			}
+		}
 	}
 }
